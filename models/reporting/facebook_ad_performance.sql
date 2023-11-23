482b6dacdd068be9acbd2130a16b8de3f0a45f25
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-11-23 14:50:51 +0100

    Create facebook_ad_performance.sql

---
+++ models/reporting/facebook_ad_performance.sql
@@ -0,0 +1,27 @@
+{{ config (
+    alias = target.database + '_facebook_ad_performance'
+)}}
+
+SELECT 
+campaign_name,
+campaign_id,
+campaign_effective_status,
+campaign_type_default,
+adset_name,
+adset_id,
+adset_effective_status,
+audience,
+ad_name,
+ad_id,
+ad_effective_status,
+visual,
+copy,
+format_visual,
+visual_copy,
+date,
+date_granularity,
+spend,
+impressions,
+link_clicks,
+complete_registration
+FROM {{ ref('facebook_performance_by_ad') }}

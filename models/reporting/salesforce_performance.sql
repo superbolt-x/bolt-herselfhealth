623f0e3d3a93f04e38e304738e26bff564d25691
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-06-06 13:16:49 -0400

    Update salesforce_performance.sql
    updated data

--- models/reporting/salesforce_performance.sql
+++ models/reporting/salesforce_performance.sql
@@ -2,60 +2,59 @@
     alias = target.database + '_salesforce_performance'
 )}}
 
-SELECT date_trunc('day', create_date :: date) :: date as date, 
-        'day' as date_granularity, 
-        preferred_clinic,
-        lead_source,
-        Lead_status,
-        disqualification_reason,
-        coalesce(count(distinct lead_id),0) as leads
-FROM {{ source('gsheet_raw','salesforce_performance') }}
-group by 1,2,3,4,5,6
+WITH data as
+    (SELECT create_date::date as date, preferred_clinic, lead_source, COUNT(DISTINCT lead_id) as leads, 0 as appointments
+    FROM {{ source('gsheet_raw','salesforce_lead_generation') }}
+    GROUP BY 1,2,3
+    UNION ALL
+    SELECT create_date::date as date, preferred_clinic, person_account_lead_source as lead_source, 0 as leads, COUNT(*) as appointments
+    FROM {{ source('gsheet_raw','salesforce_scheduled_visits') }}
+    GROUP BY 1,2,3)
+    
+SELECT DATE_TRUNC('day', date) as date, 'day' as date_granularity, 
+    preferred_clinic,
+    lead_source,
+    COALESCE(SUM(leads),0) as leads,
+    COALESCE(SUM(appointments),0) as appointments
+FROM data
+GROUP BY 1,2,3,4
 
 UNION ALL
 
-SELECT date_trunc('week', create_date :: date) :: date as date, 
-        'week' as date_granularity, 
-        preferred_clinic,
-        lead_source,
-        Lead_status,
-        disqualification_reason,
-        coalesce(count(distinct lead_id),0) as leads
-FROM {{ source('gsheet_raw','salesforce_performance') }}
-group by 1,2,3,4,5,6
+SELECT DATE_TRUNC('week', date) as date, 'week' as date_granularity, 
+    preferred_clinic,
+    lead_source,
+    COALESCE(SUM(leads),0) as leads,
+    COALESCE(SUM(appointments),0) as appointments
+FROM data
+GROUP BY 1,2,3,4
 
 UNION ALL
 
-SELECT date_trunc('month', create_date :: date) :: date as date, 
-        'month' as date_granularity, 
-        preferred_clinic,
-        lead_source,
-        Lead_status,
-        disqualification_reason,
-        coalesce(count(distinct lead_id),0) as leads
-FROM {{ source('gsheet_raw','salesforce_performance') }}
-group by 1,2,3,4,5,6
+SELECT DATE_TRUNC('month', date) as date, 'month' as date_granularity, 
+    preferred_clinic,
+    lead_source,
+    COALESCE(SUM(leads),0) as leads,
+    COALESCE(SUM(appointments),0) as appointments
+FROM data
+GROUP BY 1,2,3,4
 
 UNION ALL
 
-SELECT date_trunc('quarter', create_date :: date) :: date as date, 
-        'quarter' as date_granularity, 
-        preferred_clinic,
-        lead_source,
-        Lead_status,
-        disqualification_reason,
-        coalesce(count(distinct lead_id),0) as leads
-FROM {{ source('gsheet_raw','salesforce_performance') }}
-group by 1,2,3,4,5,6
+SELECT DATE_TRUNC('quarter', date) as date, 'quarter' as date_granularity, 
+    preferred_clinic,
+    lead_source,
+    COALESCE(SUM(leads),0) as leads,
+    COALESCE(SUM(appointments),0) as appointments
+FROM data
+GROUP BY 1,2,3,4
 
 UNION ALL
 
-SELECT date_trunc('year', create_date :: date) :: date as date, 
-        'year' as date_granularity, 
-        preferred_clinic,
-        lead_source,
-        Lead_status,
-        disqualification_reason,
-        coalesce(count(distinct lead_id),0) as leads
-FROM {{ source('gsheet_raw','salesforce_performance') }}
-group by 1,2,3,4,5,6
+SELECT DATE_TRUNC('year', date) as date, 'year' as date_granularity, 
+    preferred_clinic,
+    lead_source,
+    COALESCE(SUM(leads),0) as leads,
+    COALESCE(SUM(appointments),0) as appointments
+FROM data
+GROUP BY 1,2,3,4
